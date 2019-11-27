74b0ca87d2b865cf0aa9ecfab913443a16f1cc30
Carlos <cfelixbeltran@dynamicagency.com>
2019-11-27 08:38:02 -0600

    REFACTORED the rendering of App

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Card from './Components/Card';
+import App from './App'
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
-import { contacts } from './contactData';
+
 
 ReactDOM.render(
-    <div>
-        <Card id={contacts[0].id} name={contacts[0].name} email={contacts[0].email} />
-        <Card id={contacts[1].id} name={contacts[1].name} email={contacts[1].email} />
-        <Card id={contacts[2].id} name={contacts[2].name} email={contacts[2].email} />
-    </div>
+    <App />
 , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
