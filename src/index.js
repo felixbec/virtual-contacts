1ca176c14fb911919b76f0ace40d3d6206b0a27f
Carlos <cfelixbeltran@dynamicagency.com>
2019-11-26 20:57:03 -0600

    INITIAL Card Component implementation

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Card from './Components/Card';
 import * as serviceWorker from './serviceWorker';
+import 'tachyons';
+import { contacts } from './contactData';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <div>
+        <Card id={contacts[0].id} name={contacts[0].name} email={contacts[0].email} />
+        <Card id={contacts[1].id} name={contacts[1].name} email={contacts[1].email} />
+        <Card id={contacts[2].id} name={contacts[2].name} email={contacts[2].email} />
+    </div>
+, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
