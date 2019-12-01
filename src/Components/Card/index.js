54edc12ac17b5570dfea7425e471859792505bf4
Carlos <cfelixbeltran@dynamicagency.com>
2019-11-30 19:23:38 -0600

    MODIFIED avatar image source

--- src/Components/Card/index.js
+++ src/Components/Card/index.js
@@ -4,7 +4,7 @@ const Card = (props) => {
     const {id, name, email} = props;
     return(
         <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-            <img alt="Contact" src={`https://robohash.org/${id}?200x200`} />
+            <img alt="Contact" src={`https://i.pravatar.cc/200?u=${id}`} />
             <div>
                 <h2>{name}</h2>
                 <p>{email}</p>
